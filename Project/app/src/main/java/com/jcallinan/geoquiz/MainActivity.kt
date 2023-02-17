9a00225a01fc0f05b1d6f28d0c302708d99e9521
Jeremy Callinan <jeremy.callinan@gmail.com>
2023-02-16 19:59:19 -0500

    Completion

--- Project/app/src/main/java/com/jcallinan/geoquiz/MainActivity.kt
+++ Project/app/src/main/java/com/jcallinan/geoquiz/MainActivity.kt
@@ -2,26 +2,67 @@ package com.jcallinan.geoquiz
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
 import android.widget.Button
 import android.widget.Toast
-
+import androidx.activity.viewModels
+import com.jcallinan.geoquiz.databinding.ActivityMainBinding
+private const val TAG = "MainActivity"
 class MainActivity : AppCompatActivity() {
-    private lateinit var trueButton: Button
-    private lateinit var falseButton: Button
+    private lateinit var binding: ActivityMainBinding
+    private val quizViewModel:QuizViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        trueButton = findViewById(R.id.true_button)
-        falseButton = findViewById(R.id.false_button)
-
-        trueButton.setOnClickListener { view: View ->
-            Toast.makeText(this,R.string.correct_toast, Toast.LENGTH_SHORT).show()
+       binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        Log.d(TAG,"Got a QuizViewModel: $quizViewModel")
+        binding.trueButton.setOnClickListener { view: View ->
+            checkAnswer(true)
         }
-        falseButton.setOnClickListener { view: View ->
-            Toast.makeText(this,R.string.incorrect_toast, Toast.LENGTH_SHORT).show()
+        binding.falseButton.setOnClickListener { view: View ->
+            checkAnswer(false)
         }
+        binding.nextButton.setOnClickListener{
+            quizViewModel.moveToNext()
+            updateQuestion()
+
+        }
+        updateQuestion()
+    }
+    private fun updateQuestion() {
+        val questionTextResId = quizViewModel.currentQuestionText
+        binding.questionTextView.setText(questionTextResId)
+    }
+    private fun checkAnswer(userAnswer: Boolean) {
+        var correctAnswer = quizViewModel.currentQuestionAnswer
+        val messageResId = if (userAnswer == correctAnswer) {
+            R.string.correct_toast
+        } else {
+            R.string.incorrect_toast
+        }
+        Toast.makeText(this,messageResId,Toast.LENGTH_LONG).show()
+
+    }
+    override fun onStart() {
+        super.onStart()
+        Log.d(TAG, "onStart() called")
+    }
+    override fun onResume() {
+        super.onResume()
+        Log.d(TAG, "onResume() called")
+    }
+    override fun onPause() {
+        super.onPause()
+        Log.d(TAG, "onPause() called")
+    }
+    override fun onStop() {
+        super.onStop()
+        Log.d(TAG, "onStop() called")
+    }
+    override fun onDestroy() {
+        super.onDestroy()
+        Log.d(TAG, "onDestroy() called")
     }
 }
